6897624ae2b6f6aa459eb9c3251735ef618cae78
Jordy4200 <jordyvelasco3322@gmail.com>
2024-07-03 21:09:15 -0600

    LoginScreen

---
+++ JuegoMoviles/screens/LoginScreen.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export default function LoginScreen({ navigation }: any) {
+ 
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}></Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Usuario"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate("")}>
+        <Text style={styles.btnText}>Iniciar Sesión</Text>
+      </TouchableOpacity>
+      <TouchableOpacity  onPress={()=> navigation.navigate("")}>
+        <Text style={styles.btnText}>Registrarse</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#587e63',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    color: '#181716',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+    height: 50,
+    width: '80%',
+    borderRadius: 20,
+    margin: 10,
+    paddingHorizontal: 10,
+  },
+  btn: {
+    backgroundColor: '#41904a',
+    width: '70%',
+    height: 70,
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  // ...resto de tus estilos
+});
