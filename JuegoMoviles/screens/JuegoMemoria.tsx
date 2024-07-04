f4435c41cda7767a05d71a90561deeca07205c43
codemasterg19 <pablo_jmz1@hotmail.com>
2024-07-03 20:38:59 -0500

    screens

---
+++ JuegoMoviles/screens/JuegoMemoria.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function JuegoMemoria() {
+  return (
+    <View>
+      <Text>JuegoMemoria</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
