d855e77aefc5362455927662e9291a23e7f77989
codemasterg19 <pablo_jmz1@hotmail.com>
2024-07-09 20:59:19 -0500

    archivo de configuracion

--- JuegoMoviles/config/Config.tsx
+++ JuegoMoviles/config/Config.tsx
@@ -2,6 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage} from "firebase/storage";
+import { getAuth,} from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +24,8 @@ const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 //export const auth = getAuth(app);
 export const storage = getStorage(app);
+
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  });
 
